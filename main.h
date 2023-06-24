997799e6ef61f4173d1881544d33771f04ef8df0
root <yskubukarim12@gmail.com>
2023-06-24 08:03:42 -0700

    main header file

--- main.h
+++ main.h
@@ -5,16 +5,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 
 typedef struct check
 {
 	char *sp;
-	int *(f)(va_list);
+	int (*f)(va_list);
 } check_sp;
 
 int _printf(const char *format, ...);
-int *(check_specifier(char *specifier))(va_list);
+int (*check_specifier(const char *specifier))(va_list);
 
 int print_char(va_list args);
 int print_string(va_list args);
+int print_percent(va_list args);
 #endif
