fd551c689d25a4eda29bfe09998975430c16c92e
root <yskubukarim12@gmail.com>
2023-06-25 21:16:24 -0700

    print integers into binary

---
+++ print_binary.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * print_binary - Function that prints integers in binary.
+ * @args: argument lists.
+ *
+ * Return: Number of characters printed
+ */
+int print_binary(va_list args)
+{
+	int count = 0, i, index = 0, length, value;
+	unsigned int binary[32];
+	char buffer[32];
+
+	unsigned int b = va_arg(args, unsigned int);
+
+	if (b == 0)
+	{
+		buffer[index] = b + '0';
+		value = write(1, buffer, 1);
+		count += value;
+	}
+
+	else
+	{
+		for (i = 0; b > 0; i++)
+		{
+			binary[i] = b % 2;
+			b /= 2;
+		}
+
+		length = i;
+		for (i = length - 1; i >= 0; i--)
+		{
+			buffer[index] = binary[i] + '0';
+			index++;
+		}
+
+		value = write(1, buffer, length);
+		count += value;
+	}
+	return (count);
+}
