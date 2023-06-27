ee8ba596b45b10dfed75b8e5f8f193ca42afd5c6
root <yskubukarim12@gmail.com>
2023-06-27 05:31:52 -0700

    update

--- print_percent.c
+++ print_percent.c
@@ -13,14 +13,15 @@ int print_percent(va_list args)
 
 	pc = va_arg(args, int);
 	count = 0;
-
-	if (pc)
+	
+	if (pc != '\0')
 	{
 		value = write(1, "%%", 1);
+		if (value < 0)
+			return (-1);
 
 		count += value;
 		return (count);
 	}
-
 	return (0);
 }
